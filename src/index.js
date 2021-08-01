8637c2830b251d5a438b8337ae221338ff7d8261
IiIymik <sambotemp@gmail.com>
2021-08-01 20:40:16 +0300

    sddasd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import './index.css';
 import './utils/colorSwitch.js';
+import './utils/colorSwitch.ts';
 
 // ReactDOM.render(<App />, document.querySelector('#root'));
 ReactDOM.render(
